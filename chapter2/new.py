cac939b38420f9c153f6ec794e9e17c45fb9d75c
kiranr12r <rkiru04@gmail.com>
2026-06-21 22:29:35 +0530

    new

---
+++ chapter2/new.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the value for A :- "))
+b = int(input("Enter the value for B :- "))
+
+c = a+b
+print("the sum of two numbers from A and B is :- " , c)
--- chapter2/problem1.py
+++ chapter2/problem1.py
@@ -3,3 +3,17 @@ b = int(input("Enter the value for B :- "))
 
 c = a+b
 print("the sum of two numbers from A and B is :- " , c)
+
+
+a = int(input("Enter the value for A :- "))
+b = int(input("Enter the value for B :- "))
+
+c = a+b
+print("the sum of two numbers from A and B is :- " , c)
+
+
+a = int(input("Enter the value for A :- "))
+b = int(input("Enter the value for B :- "))
+
+c = a+b
+print("the sum of two numbers from A and B is :- " , c)
